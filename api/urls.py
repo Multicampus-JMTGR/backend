2a3d67024a0c228659dd29144eb35dbe8cd5332a
minnczi <kmj1995kr@gmail.com>
2020-11-02 14:38:30 +0900

    certificates api created

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from .views import HelloAPI
+from . import views
+# from rest_framework import routers
+# from .api import CertificateViewSet
 
 urlpatterns = [
-    path("", HelloAPI),
+    path('', views.ListCertificates.as_view()),
+    path('<int:pk>/', views.DetailCertificates.as_view()),
+
 ]
