72bf13dd2eaabc165f30f3d0e52f2f2ce8593a9a
sunyeongchoi <sn0716@naver.com>
2020-11-02 17:54:19 +0900

    user-api작성

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from api.models import Certificate
+from api.models import Certificate, User
 
 class CertificateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Certificate
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
         fields = '__all__'
