688cdc5b3f3452e299149f7aaa5c154fc6da88a5
kmj1995kr <32619843+kmj1995kr@users.noreply.github.com>
2020-11-03 14:30:10 +0900

    cert schedule table added and fields updated

--- api/models.py
+++ api/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser, User
+from datetime import datetime
 
 
 
@@ -68,26 +69,6 @@ class Certificate(models.Model):
 
 #     class Meta:
 #         db_table = "CERTLIKES"
-gi
-# 자격증 접수일정 정보
-class CertSchedule:
-    # schedule_id = models.AutoField()
-    cert_id = models.ForeignKey(Certificate, on_delete=models.CASCADE) #FK(자격증PK)
-    test_round = models.IntegerField(max_length=50) #회차(숫자?)
-    test_type = models.CharField(max_length=10) #필기/실기
-    regdate = models.DateField #접수 날짜
-    testdate = models.DateField #시험 날짜
-    resultdate = models.DateField #결과 날짜
-    
-
-    class Meta:
-        db_table = "CERT_SCHEDULE"
-        unique_together = ['cert_id', 'test_round', 'test_type']
-
-    def __str__(self):
-        return self.name
-
-
 
 
 
@@ -97,7 +78,7 @@ class StudyPlan(models.Model):
     # content_id = models.AutoField() #PK(스터디플랜PK)
     id_token = models.ForeignKey(User, on_delete=models.CASCADE) #FK(사용자PK)
     cert_id = models.ForeignKey(Certificate, on_delete=models.CASCADE) #FK(자격증PK)
-    date = models.DateField #달력에서 날짜 부분
+    date = models.DateField(blank=True, null=True) #달력에서 날짜 부분
     contents = models.CharField(max_length=1000) #todolist작성내용
 
     class Meta:
@@ -107,8 +88,23 @@ class StudyPlan(models.Model):
         return self.contents
 
 
+# 자격증 접수일정 정보
+class CertSchedule(models.Model):
+    # schedule_id = models.AutoField()
+    cert_id = models.ForeignKey(Certificate, on_delete=models.CASCADE) #FK(자격증PK)
+    test_round = models.IntegerField() #회차(숫자?)
+    test_type = models.CharField(max_length=10) #필기/실기
+    reg_start_date = models.DateField(blank=True, null=True) #접수 시작 날짜
+    reg_end_date = models.DateField(blank=True, null=True) #접수 마감 날짜
+    test_start_date = models.DateField(blank=True, null=True) #시험 시작 날짜
+    test_end_date = models.DateField(blank=True, null=True) #시험 마감 날짜
+    result_date_1 = models.DateField(blank=True, null=True) #결과 날짜
+    result_date_2 = models.DateField(blank=True, null=True) #추가적인 결과 날짜가 있으면
+    
+    class Meta:
+        db_table = "CERT_SCHEDULE"
+        # 장고에서는 compound PK 지정이 안되서 pk는 자동생성되는 인덱스 값을 만들고 유니크한 필드값을 지정
+        unique_together = ['cert_id', 'test_round', 'test_type'] 
 
-
-
-
-
+    def __str__(self):
+        return self.cert_id
