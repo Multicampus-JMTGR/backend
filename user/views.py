abaaf0a08dbee9e64f3c8d6d7af49f58459f9cd6
minnczi <kmj1995kr@gmail.com>
2020-11-06 15:27:20 +0900

    changes to user filter

--- user/views.py
+++ user/views.py
@@ -39,30 +39,64 @@ def UserAPIDetail(request, pk):
         serializer = UserSerializer(queryset)
         return Response(serializer.data)
 
+# class UserAPIDetail(generics.RetrieveUpdateDestroyAPIView):
+#     """
+#     유저정보, 불러오기, 삭제하기, 수정하기
+#     """
+#     @api_view(['GET'])
+#     def get(request, pk):
+#         try:
+#             user = User.objects.get(email=pk)
+#             serializer = UserSerializer(user)
+#             return Response(serializer.data)
+#         except User.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+    
+#     def update(request, pk, cert_id):
+#         user = User.objects.get(email=pk)
+#         serializer = UserSerializer(instance=user, data=request.data)
+#         if serializer.is_valid():
+#             check_like_post = serializer.object.cert_likes.filter(cert_likes=cert_id)
+#             if check_like_post == True:
+#                 serializer.object.cert_likes.remove(cert_id)
+#                 serializer.save()
+#                 return Response(serializer.data, status=status.HTTP_201_CREATED)
+#             else:
+#                 serializer.object.cert_likes.add(cert_id)
+#                 serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
 @api_view(['GET', 'POST'])
 def CertificateLikeAPI(request, pk, cert_id):
     # pk값과 일치하는 유저정보 불러오기
     user = User.objects.get(email=pk)
     serializer = UserSerializer(instance=user, data=request.data)
-
     # 유저 instance중 cert_likes 필드에 cert_id가 이미 존재하는지 확인하기
-    # check_like_post = serializer.object.cert_likes.filter(cert_likes=cert_id)
     if request.method == 'GET':
         if serializer.is_valid():
             return Response(serializer.data)
 
     elif request.method == 'POST':
+        check_like_post = serializer.objects.cert_likes.filter(cert_likes=cert_id)
         if serializer.is_valid():
             # check_like_post가 존재한다면, 자격증 없애기
             if check_like_post == True:
-                serializer.object.cert_likes.remove(cert_id)
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
+                user.cert_likes.remove(cert_id)
+                user.save()
+                return Response(serializer.data)
             # check_like_post가 없다면, 자격증 추가하기
             else:
-                serializer.object.cert_likes.add(cert_id)
-                serializer.save()
+                user.cert_likes.add(cert_id)
+                user.save()
+                return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 # 스터디 플랜 정보가 어차피 유저 안에 포함되 있어서 이게 따로 필요할진 모르겠음 일단 주석처리!
 # # Study Plan List
